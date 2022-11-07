cfcc908bcb456fc80771b2855606be9248723be0
fahad Mridha <sohelcse16@gmail.com>
2022-11-08 01:55:00 +0600

    router setting for all section

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../shared/footer/Footer";
+import Header from "../shared/header/Header";
 
 const Main = () => {
   return (
     <div>
-      <h2>this is main</h2>
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
--- src/route/Route.js
+++ src/route/Route.js
@@ -1,9 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import ErrorPage from "../pages/error/ErrorPage";
+import Home from "../pages/home/Home";
+import Services from "../pages/services/Services";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+    ],
   },
 ]);
