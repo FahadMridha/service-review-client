9bfbfc079241dec59172a4f71e6a1a3d2459c582
fahad Mridha <sohelcse16@gmail.com>
2023-02-23 03:10:28 +0600

    home section update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -23,6 +23,20 @@ const Home = () => {
     <div className="m-8">
       <Banner />
       <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+          quisquam laudantium numquam mollitia asperiores enim, dolore porro
+          nihil, quibusdam odit molestias accusantium tempora nam doloremque
+          adipisci, neque debitis dolor in? Totam aliquam obcaecati architecto
+          praesentium quo omnis rem quia alias repellat est aspernatur et
+          repellendus temporibus fugit, unde autem maiores? Ab nemo repellendus
+          reprehenderit maxime rerum earum, nulla vel consequatur eos, eius
+          minima architecto ducimus quos officiis voluptates modi, sapiente
+          expedita sit quibusdam. Minus quibusdam vel ducimus, et in harum a quo
+          suscipit. Magnam, ullam. Nihil dicta, quasi possimus unde quod,
+          asperiores odio eius, recusandae nesciunt laborum laboriosam incidunt
+          a!
+        </p>
         <p className="text-center text-5xl font-bold my-12 ">My Services</p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-8">
           {services.map((service) => (
